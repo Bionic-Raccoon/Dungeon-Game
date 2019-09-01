22be2f04ef3a7cc227f5d4fce7f311d09de89271
Bionic-Raccoon <54753024+Bionic-Raccoon@users.noreply.github.com>
2019-09-01 17:02:51 -0400

    Update fightmonsters-generic.py

--- fightmonsters-generic.py
+++ fightmonsters-generic.py
@@ -5,177 +5,277 @@ import string
 
 
 def statdescriptor(stat):
-	if stat == 3:
+	""" This is just a function to return 'friendly' names for stats"""
+	if stat > 4:
+		return "godly"
+	elif stat == 4:
+		return "super"
+	elif stat == 3:
 		return "very"
 	elif stat == 2:
 		return "sort of"
 	elif stat == 1:
 		return "kind of"
-	else:
+	elif stat == 0:
 		return "not very"
+	else:
+		return "the opposite of"
+		
+def showstats():
+	""" This function prints out the character's stat blocks """
+	print ("")
+	print ("====================")
+	print ("Name: " + name)#4FCAA3
+	print ("Strength: " + statdescriptor(strength) + " strong")
+	print ("Dexterity: " + statdescriptor(dexterity) + " dextrous")
+	print ("Constitution: " + statdescriptor(constitution) + " tough")
+	print ("Hit Points: " + str(hitpoints) + " (of " + str(maxhitpoints) + ")")
+	print ("Armor Class: " + str(armorclass))
+	print ("====================\n")
+	sleep(3)
 
-def fightmonster(monster="", monstermaxhp=1, monsterac = 10, monsterxp=0, monsterdamage=1, hitpoints=0, kills=0, xp=0):
-	# This is a generic fight function, you can tell it what to fight
+def fightmonster(monster="", monstermaxhp=1, monsterac = 10, monsterxp=0, monsterdamage=1, monsterhitbonus = 0, monsterdamagebonus = 0, hitpoints=0, kills=0, xp=0):
+	""" This is the main fighting part of the program """
+	showstats()
 	monsterhp = random.randint(1,monstermaxhp)
-	print ("You encounter a " + monster + "!")
+	print ("\n" + name + " encounters a " + monster + "!")
 	sleep(1)
 	print ("It has " + str(monsterhp) + " hit points!")
-	print ("You have " + str(hitpoints) + "  hit points!")
+	print (name + " has " + str(hitpoints) + "  hit points!")
 	while monsterhp > 0 and hitpoints > 0:
-		attack = input("Do you want to attack? (y/n)")
+		attack = "x"
+		while attack not in ["y","n"]:
+			attack = input("Do you want to attack? (y/n)")
 		sleep(1)
 		if attack == 'y':
-			print ("You try and hit the " + monster+ "!")
+			doubledamage = False
+			print (name + " tries and hit the " + monster+ "!")
 			sleep(1)
-			tohit = random.randint(1,20) + strength
-			print ("You roll a " + str(tohit) + "!!!!")
+			tohit = random.randint(1,20)
+			if  tohit == 20:
+				doubledamage = True
+				print ("\n***Critical hit!!!***\n")
+				sleep(2)
+			tohit = tohit + strength
+			print (name + " rolls a " + str(tohit) + "!!!!")
+			if tohit == 1:
+				print("Really!?!  A ONE!!!")
+				sleep(2)
 			sleep(1)
 			if tohit > monsterac:
-				print ("You hit the " + monster + "!")
+				print (name + " hits the " + monster + "!")
 				sleep(1)
 				damage = random.randint(1,12) + strength
+				if doubledamage:
+					damage = damage * 2
+					doubledamage = False
 				monsterhp = monsterhp - damage
 				if monsterhp < 1:
-					print ("You killed the " + monster + "!")
+					print (name + " kills the " + monster + "!")
 					sleep(1)
 					kills = kills + 1
 					xp = xp + monsterxp
 					break
 				else:
-					print("You hit for " + str(damage) + ", the " + monster + ", it has " + str(monsterhp) + " hit points left")
+					print(name + " hits for " + str(damage) + ", the " + monster + ", it has " + str(monsterhp) + " hit points left")
 					sleep(1)
 			else:
-				print ("Your pitiful attempt fails!  You miss the " + monster + "!")
+				print (name + "'s pitiful attempt fails!  " + name + " misses the " + monster + "!")
 				sleep(1)
 		else:
 			# This is if you don't enter 'y' when asked if you want to attack
-			print("You try and get away!")
+			print(name + " tries and get away!")
 			escape = random.randint(1,10) + dexterity
 			if escape > 5:
-				print ("You get away!")
+				print (name + " gets away!")
 				return [hitpoints, kills, xp]
 			else:
-				print("The " + monster + " is too fast, you can't get away!")
-		print ("The " + monster + " tries to smite you!")
+				print("The " + monster + " is too fast, " + name + " can't get away!")
+		print ("The " + monster + " tries to smite " + name + "!")
 		sleep(1)
-		if random.randint(1,20) > 10 + dexterity:
+		if random.randint(1,20) > armorclass:
 			damage = random.randint(1,monsterdamage)
-			print ("The " + monster + " hits you for " + str(damage) + " damage!")
+			print ("The " + monster + " hits " + name + " for " + str(damage) + " damage!")
 			sleep(1)
 			hitpoints = hitpoints - damage
-			print ("You have " + str(hitpoints) + " hit points left!")
+			print (name + " has " + str(hitpoints) + " hit points left! (of " + str(maxhitpoints) + ")")
 			sleep(1)
 		else:
-			print ("The " + monster + " misses you!")
+			print ("The " + monster + " misses " + name + "!")
 			sleep(1)
 	return [hitpoints, kills, xp]
-
 	
-# The program actually starts here!
-name = input("Welcome to the adventure!\nEnter your new character's name: ")
-print ("Your new characters is named: " + name)
-sleep(1)
-
-# Easter egg
-if name.lower() == "alfonso pipethin":
-	print("Hello Dad!") 
+keepplaying = True
+while (keepplaying):	
+	# The program actually starts here!
+	name = input("Welcome to the adventure!\nEnter your new character's name: ")
+	print ("Your new characters is named: " + name)
 	sleep(1)
-	print("You are not allowed to play my game")
+
+	# Easter egg
+	if name.lower() == "alfonso pipethin":
+		print("Hello Dad!") 
+		sleep(1)
+		print("You are not allowed to play my game")
+		sleep(1)
+		print("HEHEHEHEHEHEHE")
+		sleep(1)
+		exit(0)
+
+	# Determine the character's stats
+	strength = random.randint(0,3)
+	dexterity = random.randint(0,3)
+	constitution = random.randint(0,3)
+	hitpoints = random.randint(5,10) + constitution
+	armorclass = 10 + dexterity
+	maxhitpoints = hitpoints
+	kills = 0
+	xp = 0
+	turn = 1
+	level = 1
+
+	# Test and debug names
+	if name == "Claire":
+		password = input("What is the secret number?")
+		if password == str(1397):
+			strength = 5
+			dexterity = 5
+			constitution = 5
+			hitpoints = 100
+		else:
+			print("Nice try sucka!  You get a normal character....")
+			sleep(2)
+			print("On second thought,  your character just got worse! (womp womp)")
+			strength = strength - 1
+			dexterity = dexterity - 1
+			constitution = constitution -1
+	elif name.lower() == "trash":
+		strength = 0
+		dexterity = 0
+		constitution = 0
+		hitpoints = 1
+
+	showstats()
+	
 	sleep(1)
-	print("HEHEHEHEHEHEHE")
-	sleep(1)
-	exit(0)
 
-	
-strength = random.randint(0,3)
-dexterity = random.randint(0,3)
-constitution = random.randint(0,3)
-hitpoints = random.randint(5,10) + constitution
-
-if name == "Claire":
-	strength = 3
-	dexterity = 3
-	constitution = 3
-	hitpoints = 100
-	
-
-print ("Here are your stats:")
-sleep(1)
-print ("Strength: " + statdescriptor(strength) + " strong")
-sleep(1)
-print ("Dexterity: " + statdescriptor(dexterity) + " dextrous")
-sleep(1)
-print ("Constitution: " + statdescriptor(constitution) + " tough")
-sleep(1)
-print ("Hit Points: " + str(hitpoints))
-maxhitpoints = hitpoints
-sleep(1)
-kills = 0
-xp = 0
-turn = 1
-level = 1
-
-print ("\nYou enter the dungeon!")
-	
-while hitpoints > 0:
-	if turn == 1:
-		print ("This is the 1st turn")
-	elif turn == 2:
-		print ("This is the 2nd turn")
-	elif turn == 3:
-		print ("This is the 3rd turn")
-	else:
-		print ("This is the " + str(turn) + "th turn")	
-	if turn > 1 and (turn % 3) == 0:
-		############################Healing#################################
-		healme = input("Do you want to heal yourself? (y/n)")
-		if healme == 'y':
-			healpoints = random.randint(1,2)
-			hitpoints = hitpoints + healpoints
-			if hitpoints > maxhitpoints:
+	print ("\n" + name + " enters the dungeon!")
+		
+	while hitpoints > 0:
+		if turn == 1:
+			print ("This is the 1st turn")
+		elif turn == 2:
+			print ("This is the 2nd turn")
+		elif turn == 3:
+			print ("This is the 3rd turn")
+		else:
+			print ("This is the " + str(turn) + "th turn")	
+		if turn > 1 and (turn % 3) == 0:
+			############################Healing#################################
+			healme = "x"
+			while healme not in ['y','n']: 
+					healme = input("Do you want to heal " + name +  "? (y/n)")
+			if healme == 'y':
+				healpoints = random.randint(1,2)
+				hitpoints = hitpoints + healpoints
+				if hitpoints > maxhitpoints:
+					hitpoints = maxhitpoints
+				print("You healed " + name +  "!  " + name +  " now has " + str(hitpoints) + " hit points!")#F17474
+			else:
+				# Player is a jerk, so we'll heal the character anyway!
+				print("Why not?!?  What's wrong with you?!?\n")
+				sleep(2)
+				print( name + " decides you're not the boss and heals anyway!")
 				hitpoints = maxhitpoints
-			print("You healed yourself!  You now have " + str(hitpoints) + " hit points!")
-	print("\n\nYou have " + str(((level-1)*100) + xp) + " experience points!")
-	randmonster = random.randint(1,10)
-	if randmonster < 5:
-		sleep(1)
-		[hitpoints, kills, xp] = fightmonster("goblin", 10, 12, 25, 6, hitpoints, kills, xp)
-		sleep(1)
-	elif randmonster > 4 and randmonster < 7:
-		sleep(1)
-		[hitpoints, kills, xp] = fightmonster("giant spider", 15, 13, 40, 8, hitpoints, kills, xp)
-		sleep(1)
-	elif randmonster == 7:
-		sleep(1)
-		[hitpoints, kills, xp] = fightmonster("ogre", 20, 14, 50, 12, hitpoints, kills, xp)
-		sleep(1)
-	elif randmonster == 8:
-		sleep(1)
-		[hitpoints, kills, xp] = fightmonster("a GIANT", 40, 15, 90, 16, hitpoints, kills, xp)
-		sleep(1)
-	else:
-		print ("You wander the dungeon for a while...")
-		sleep(1)
-	
-	print ("You have killed " + str(kills) + " monsters!")
-	turn = turn + 1
-	
-	if xp > 100:
-		# Level up
-		print("You gained a level!  You are now level " + str(level+1))
-		print("You can increase one of your abilities!")
-		choice = input("Which do you want to increase? (str/dex/con)")
-		if choice == "str":
-			strength = strength + 1
-		elif choice == "dex":
-			dexterity = dexterity + 1
-		elif "con":
-			constitution = constitution + 1
-		print ("Your hit points increase!")
-		hp = hp + random.randint(1-6) + constitution
-		xp = xp - 100
-	
-print ("Sorry, you died! But you killed " + str(kills) + " monsters!")
-sleep(1)
-print ("You survived " + str(turn) + " turns!")
-print ("\n so there's that...")
+				print( name + " now has " + str(hitpoints) + " (of " + str(maxhitpoints) + ")")
+				sleep(3)
+		print("\n\n" + name +  " has " + str(((level-1)*100) + xp) + " experience points!")
+		print("(need 100 for next level)")
+		randmonster = random.randint(1,11)
+		# Low level monsters
+		if level < 4:
+			if randmonster < 5:
+				sleep(1)
+				[hitpoints, kills, xp] = fightmonster("goblin", 6, 12, 25, 6, -1, -1, hitpoints, kills, xp)
+				sleep(1)
+			elif randmonster > 4 and randmonster < 7:
+				sleep(1)
+				[hitpoints, kills, xp] = fightmonster("giant spider", 15, 13, 40, 8, 1, 1, hitpoints, kills, xp)
+				sleep(1)
+			elif randmonster == 7:
+				sleep(1)
+				[hitpoints, kills, xp] = fightmonster("ogre", 20, 14, 50, 12, 3, 3, hitpoints, kills, xp)
+				sleep(1)
+			elif randmonster ==8:
+				sleep(1)
+				[hitpoints, kills, xp] = fightmonster("hill giant", 40, 15, 90, 16, 5, 5, hitpoints, kills, xp)
+				sleep(3)
+			else:
+				print ("\n" + name + " wanders the dungeon for a while...\n")
+				sleep(3)
+		# High level monsters
+		else:
+			if randmonster < 5:
+				sleep(1)
+				[hitpoints, kills, xp] = fightmonster("ogre", 20, 14, 20, 12, 3, 3, hitpoints, kills, xp)
+				sleep(1)
+			elif randmonster > 4 and randmonster < 7:
+				sleep(1)
+				[hitpoints, kills, xp] = fightmonster("troll", 25, 16, 30, 16, 1, 1, hitpoints, kills, xp)
+				sleep(1)
+			elif randmonster == 7:
+				sleep(1)
+				[hitpoints, kills, xp] = fightmonster("fire giant", 40, 15, 40, 16, 5, 5, hitpoints, kills, xp)
+				sleep(1)
+			elif randmonster ==8:
+				sleep(1)
+				[hitpoints, kills, xp] = fightmonster("Dragon", 60, 18, 90, 18, 8, 8, hitpoints, kills, xp)
+				sleep(1)
+			else:
+				print ("\n" + name + " wanders the dungeon for a while...\n")
+				sleep(3)
+			
+			
+		print (name + " has killed " + str(kills) + " monsters!")
+		turn = turn + 1
+		
+		if xp > 100:
+			# Level up
+			level = level + 1
+			print("\n--------------")
+			print(name + " gained a level!  " + name +  " is now level " + str(level))
+			print("You can increase one of " + name +  "'s abilities!")
+			validchoice = False
+			# Ensure that the player chooses a thing that exists
+			while not validchoice:
+				choice = input("Which do you want to increase? (str/dex/con)")
+				if choice in ['str','dex','con']:
+					validchoice = True
+				if choice == "str":
+					strength = strength + 1
+				elif choice == "dex":
+					dexterity = dexterity + 1
+				elif choice =="con":
+					constitution = constitution + 1
+			print ("" + name +  "'s hit points increase!")
+			hp = maxhitpoints
+			hp = hp + random.randint(1,6) + constitution
+			maxhitpoints = hp
+			xp = xp - 100
+			armorclass = 10 + dexterity
+			showstats()
+			
+	print()
+	if kills > 0:
+		print ("Sorry, " + name + " died! But " + name +  " killed " + str(kills) + " monsters! " + name +  " survived " + str(turn) + " turns! \n so there's that...")
+	else: 
+		print (name + " killed 0 monsters. You should be ashamed of yourself.  " + name +  " survived " + str(turn) + " turns! \n so there's that...")
+		
+	sleep(1)
+	keepgoing = "x"
+	while keepgoing not in ['y','n']: 
+		keepgoing = input("\nDo you want to play again? (y/n)")
+	if keepgoing == 'n':
+		keepplaying = False
+		
+print ("\nGoodbye!")
